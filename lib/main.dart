9fdf3567a297aa5511e1b89b6f4e8e9c5097fcae
Holy-Morphisim <ranaadeeltahir@gmail.com>
2023-02-06 03:31:56 +0500

    Seperate Transaction Widgets Created

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
-import './transaction.dart';
-import 'package:intl/intl.dart';
+import './models/transaction.dart';
+import './widgets/transaction_list.dart';
+import './widgets/new_transaction.dart';
 
 void main() => runApp(MyApp());
 
@@ -17,29 +17,8 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
-  final List<Transaction> transactions = [
-    Transaction(
-      id: '12345',
-      title: 'chai',
-      amount: 50,
-      date: DateTime.now(),
-    ),
-    Transaction(
-      id: '12346',
-      title: 'Lays',
-      amount: 60,
-      date: DateTime.now(),
-    ),
-    Transaction(
-      id: '12347',
-      title: 'Mango Juice',
-      amount: 30,
-      date: DateTime.now(),
-    ),
-  ];
-  String amountInput, titleInput;
-  final titleController = TextEditingController();
-  final amountController = TextEditingController();
+  final List<Transaction> transactions = [];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -73,87 +52,6 @@ class MyHomePage extends StatelessWidget {
                 ),
                 padding: EdgeInsets.fromLTRB(0, 0, 0, 50),
               ),
-            ),
-            Card(
-              elevation: 10,
-              child: Container(
-                padding: EdgeInsets.all(10),
-                child: Column(children: <Widget>[
-                  TextField(
-                    decoration: InputDecoration(labelText: 'Title'),
-                    controller: amountController,
-                    onChanged: (val) {
-                      titleInput = val;
-                    },
-                  ),
-                  TextField(
-                    decoration: InputDecoration(labelText: 'Amount'),
-                    controller: amountController,
-                    onChanged: (val) {
-                      titleInput = val;
-                    },
-                  ),
-                  ElevatedButton(
-                    onPressed: null,
-                    child: Text("SAVE"),
-                    style: ButtonStyle(
-                        backgroundColor:
-                            MaterialStateProperty.all(Colors.green),
-                        foregroundColor:
-                            MaterialStateProperty.all(Colors.white),
-                        elevation: MaterialStateProperty.all(5)),
-                  )
-                ]),
-              ),
-            ),
-            Column(
-              children: transactions
-                  .map((transaction) => Card(
-                        child: Row(
-                          children: [
-                            Container(
-                              margin: EdgeInsets.symmetric(
-                                vertical: 10,
-                                horizontal: 15,
-                              ),
-                              decoration: BoxDecoration(
-                                border: Border.all(
-                                  color: Colors.purple,
-                                  width: 2,
-                                ),
-                              ),
-                              padding: EdgeInsets.all(2),
-                              child: Text(
-                                'Rs.${transaction.amount.toString()}', //String Interpolation
-                                style: TextStyle(
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: 20,
-                                    color: Colors.purple),
-                              ),
-                            ),
-                            Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: <Widget>[
-                                Text(
-                                  transaction.title.toString(),
-                                  style: TextStyle(
-                                    fontSize: 16,
-                                    fontWeight: FontWeight.bold,
-                                  ),
-                                ),
-                                Text(
-                                  DateFormat('d LLLL yyyy h:mm a')
-                                      .format(transaction.date),
-                                  style: TextStyle(
-                                    color: Colors.grey,
-                                  ),
-                                ),
-                              ],
-                            )
-                          ],
-                        ),
-                      ))
-                  .toList(),
             ),
           ],
         ));
