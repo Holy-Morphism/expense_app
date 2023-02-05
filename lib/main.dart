2023bde41e5b61daf0210b450c04c744f6793375
Holy-Morphisim <ranaadeeltahir@gmail.com>
2023-02-05 19:54:51 +0500

    Assets Added

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import './transaction.dart';
-import './cashflow.dart';
+import 'package:intl/intl.dart';
 
 void main() => runApp(MyApp());
 
@@ -36,6 +37,9 @@ class MyHomePage extends StatelessWidget {
       date: DateTime.now(),
     ),
   ];
+  String amountInput, titleInput;
+  final titleController = TextEditingController();
+  final amountController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -43,10 +47,115 @@ class MyHomePage extends StatelessWidget {
           title: Text('Flutter App'),
         ),
         body: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: transactions
-                .map((transaction) => CashFlow(title: transaction.title))
-                .toList()));
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Card(
+              margin: const EdgeInsets.all(10),
+              elevation: 5,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Container(
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('images\\tanjiro.jpeg'),
+                    fit: BoxFit.cover,
+                    alignment: Alignment.center,
+                  ),
+                ),
+                child: Text(
+                  'CHART!',
+                  textAlign: TextAlign.left,
+                  style: TextStyle(
+                    fontSize: 40,
+                    fontFamily: 'SofiaSansCondensed',
+                  ),
+                ),
+                padding: EdgeInsets.fromLTRB(0, 0, 0, 50),
+              ),
+            ),
+            Card(
+              elevation: 10,
+              child: Container(
+                padding: EdgeInsets.all(10),
+                child: Column(children: <Widget>[
+                  TextField(
+                    decoration: InputDecoration(labelText: 'Title'),
+                    controller: amountController,
+                    onChanged: (val) {
+                      titleInput = val;
+                    },
+                  ),
+                  TextField(
+                    decoration: InputDecoration(labelText: 'Amount'),
+                    controller: amountController,
+                    onChanged: (val) {
+                      titleInput = val;
+                    },
+                  ),
+                  ElevatedButton(
+                    onPressed: null,
+                    child: Text("SAVE"),
+                    style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStateProperty.all(Colors.green),
+                        foregroundColor:
+                            MaterialStateProperty.all(Colors.white),
+                        elevation: MaterialStateProperty.all(5)),
+                  )
+                ]),
+              ),
+            ),
+            Column(
+              children: transactions
+                  .map((transaction) => Card(
+                        child: Row(
+                          children: [
+                            Container(
+                              margin: EdgeInsets.symmetric(
+                                vertical: 10,
+                                horizontal: 15,
+                              ),
+                              decoration: BoxDecoration(
+                                border: Border.all(
+                                  color: Colors.purple,
+                                  width: 2,
+                                ),
+                              ),
+                              padding: EdgeInsets.all(2),
+                              child: Text(
+                                'Rs.${transaction.amount.toString()}', //String Interpolation
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 20,
+                                    color: Colors.purple),
+                              ),
+                            ),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                Text(
+                                  transaction.title.toString(),
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                Text(
+                                  DateFormat('d LLLL yyyy h:mm a')
+                                      .format(transaction.date),
+                                  style: TextStyle(
+                                    color: Colors.grey,
+                                  ),
+                                ),
+                              ],
+                            )
+                          ],
+                        ),
+                      ))
+                  .toList(),
+            ),
+          ],
+        ));
   }
 }
